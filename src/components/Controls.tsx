ec1d56cdf699a909928596eeb8c2f0080d8b2da0
asumie-code <darkeangel.1998.ia@gmail.com>
2024-05-21 20:16:29 +0100

    feat: implement controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,2 +1,83 @@
+import { Button, Paper, TextField} from "@mui/material"
+import { ChangeEvent } from "react"
 
 
+
+
+
+interface Controlprops {
+    pageNumber: number, 
+    numPages: number, 
+    setPageNumber: React.Dispatch<React.SetStateAction<number>>, 
+    scale: number, 
+    setScale: React.Dispatch<React.SetStateAction<number>>
+}
+
+const Controls = (controlProps: Controlprops): JSX.Element => {
+    
+    const { pageNumber, numPages, setPageNumber, scale, setScale } = controlProps
+
+
+
+    const isFirstPage: boolean = pageNumber === 1 
+    const isLastPage: boolean = pageNumber === numPages
+
+
+    const firstPageClass = isFirstPage ? 'disabled': 'clickable'
+    const lastPageClass = isLastPage ? 'disabled' : 'clickable'
+
+
+    const goToFirstPage = (): void => {
+        if (!isFirstPage) setPageNumber(1)
+    }
+
+    const goToPreviousPage = (): void => {
+        if (!isFirstPage) setPageNumber( pageNumber - 1)
+    }
+
+    const goToNextPage = (): void => {
+        if (!isLastPage) setPageNumber(pageNumber + 1)
+    }
+
+    const goToLastPage = (): void => {
+        if (!isFirstPage) setPageNumber(numPages)
+    }
+
+    const onPageChange  = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value } = event.currentTarget
+        setPageNumber(Number(value))
+    }
+
+    const isMinZoom: boolean = scale < 0.6;
+    const isMaxZoom: boolean = scale >= 2.0;
+
+    
+    const zoomOutClass = isMinZoom ? 'disabled' : 'clickable';
+    const zoomInClass = isMaxZoom ? 'disabled' : 'clickable';
+
+
+    const zoomOut = (): void => {
+        if (!isMinZoom) setScale(scale - 0.1);
+      };
+    
+      const zoomIn = (): void => {
+        if (!isMaxZoom) setScale(scale + 0.1);
+      };
+    
+    return (
+        <Paper>
+            <Button onClick={goToFirstPage}>gottofirstpage</Button>
+            <Button onClick={goToPreviousPage}>gottopreviouspage</Button>
+            <TextField name="pageNumber" value={pageNumber} onChange={onPageChange}   label="Outlined" variant="outlined"  /> of {pageNumber}
+            <Button onClick={goToNextPage}>gottonextpage</Button>
+            <Button onClick={goToLastPage}>gottolastpage</Button>
+            <Button onClick={zoomIn}>zoomin</Button>
+            <Button onClick={zoomOut}>zoomout</Button>
+
+
+        </Paper>
+    )
+}
+
+
+export default Controls
--- src/components/Pdf.tsx
+++ src/components/Pdf.tsx
@@ -1,6 +1,10 @@
 import  { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import {useParams} from 'react-router-dom'
+import { Container } from "@mui/material";
+import Controls from "./Controls";
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 import books from "./files";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -11,8 +15,8 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 export default function Pdf() {
    const {pdf} = useParams()
   
-   const [scale, setScale] = useState(1);
-   const [numPages, setNumPages] = useState(null);
+   const [scale, setScale] = useState(2);
+   const [numPages, setNumPages] = useState(0);
    const [pageNumber, setPageNumber] = useState(1);
    const [isLoading, setIsLoading] = useState(true);
   function onDocumentLoadSuccess({ numPages }: any) {
@@ -23,18 +27,21 @@ export default function Pdf() {
 
 
   return (
-    <>
- <div>
-    
- 
-        <Document
+    <Container >
+      <Controls 
+                   scale={scale}
+                   setScale={setScale}
+                   numPages={numPages}
+                   pageNumber={pageNumber}
+                   setPageNumber={setPageNumber}
+      />
+       <Document
+          
           file={books[0]}
           onLoadSuccess={onDocumentLoadSuccess}
-        >
+       >
           <Page pageNumber={pageNumber} scale={scale} />
-        </Document>
-   
-    </div>
-    </>
+       </Document>
+    </Container>
   );
 } 
